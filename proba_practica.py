af976f3e085053b1160b2976125cb77be0dea2fd
SerbanApostol <69292798+SerbanApostol@users.noreply.github.com>
2020-08-10 08:56:23 +0300

    Added creating new storage file if it does not exist

--- proba_practica.py
+++ proba_practica.py
@@ -5,19 +5,23 @@ import os
 
 ourClient = mqtt.Client("proba_practica") # Create a MQTT client object
 ourClient.connect("mqtt.beia-telemetrie.ro", 1883) # Connect to the MQTT broker
-ourClient.subscribe("/training/device/Serban_Apostol/") # Subscribe to the topic
+ourClient.subscribe("/training/device/Lorena_Muja/") # Subscribe to the topic
 
 while True:
   temp = check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True) # Get CPU temperature
   now = time.strftime("%d-%b-%Y,%H:%M:%S") # Get time
   info = now + ' ' + str(temp[5:9])[2:6]
-  ourClient.publish("/training/device/Serban_Apostol/", info) # Publish time and CPU temperature to MQTT broker
+  ourClient.publish("/training/device/Lorena-Muja/", info) # Publish time and CPU temperature to MQTT broker
+
   # Writing to local file
-  filename = "/home/pi/Serban_Apostol/temp.txt"
+  filename = "/home/pi/Lorena-Muja/temp.txt"
   try:
     with open(filename, 'a') as f:
-        f.write(info + '\n')
-        f.close()
+      f.write(info + '\n')
+      f.close()
   except:
     print("File does not exist!")
+    f = open(filename, 'x')
+    f.write(info + '\n')
+    f.close()
   time.sleep(20) # Sleep for 20 seconds
