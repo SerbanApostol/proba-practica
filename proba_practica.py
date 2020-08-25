ef1f19c93dfe668575e68f8849667e46bb3d9c54
SerbanApostol <69292798+SerbanApostol@users.noreply.github.com>
2020-08-25 14:56:54 +0300

    Changed topic to be correct
    Also changed temperature format because it was not represented correctly

--- proba_practica.py
+++ proba_practica.py
@@ -1,15 +1,18 @@
 import paho.mqtt.client as mqtt # Import the MQTT library
 import time # The time library is useful for delays
 from subprocess import check_output # Function to return output of console commands
+import json
 import os
 
 ourClient = mqtt.Client("proba_practica") # Create a MQTT client object
 ourClient.connect("mqtt.beia-telemetrie.ro", 1883) # Connect to the MQTT broker
-ourClient.subscribe("/training/device/Lorena_Muja/") # Subscribe to the topic
+ourClient.subscribe("training/device/Serban_Apostol") # Subscribe to the topic
 
 while True:
   temp = check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True) # Get CPU temperature
   now = time.strftime("%d-%b-%Y,%H:%M:%S") # Get time
-  info = "\"Time:\" + now + ",\"CPU_temp\"" + str(temp[5:9])[2:6]
-  ourClient.publish("/training/device/Lorena_Muja/", info) # Publish time and CPU temperature to MQTT broker
+  info = {"Time":now,
+          "CPU_temp":temp[5:9]
+         }
+  ourClient.publish("training/device/Serban_Apostol", json.dumps(info)) # Publish time and CPU temperature to MQTT broker
   time.sleep(20) # Sleep for 20 seconds
