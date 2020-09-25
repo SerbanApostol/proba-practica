ac6c0241eccde428a0e7dcb5ae482b81ebdacefa
SerbanApostol <69292798+SerbanApostol@users.noreply.github.com>
2020-09-25 15:20:29 +0300

    Given up DHT sensor and replaced it with CPU temperature

--- proba_practica.py
+++ proba_practica.py
@@ -2,8 +2,6 @@ import paho.mqtt.client as mqtt # Import the MQTT client
 import time # The time library is useful for delays
 from subprocess import check_output # Function to return output of console commands
 import json # Import json to send data as json file
-import adafruit_dht
-import board
 import os
 
 def try_connecting():
@@ -31,26 +29,16 @@ ourClient = mqtt.Client("proba_practica") # Create a MQTT client object
 #try_connecting()
 #ourClient.subscribe("training/device/Serban_Apostol") # Subscribe to the topic
 
-dht_device = adafruit_dht.DHT11(board.D4)
-
 while True:
   try_connecting()
-  try:
-    temp = dht_device.temperature
-    hum = dht_device.humidity
-    if temp is not None and hum is not None:
-      info = {
-              "Temperature":round(temp,2),
-              "Humidity":round(hum,2)
-             }
-      print(info)
-      with open("/home/pi/Serban_Apostol/CPU_temp.log", "a") as log:
-        log.write(str(info)+"\n")
-      if is_connected:
-        ourClient.publish("training/device/Serban_Apostol", json.dumps(info)) # Publish time and CPU temperature to MQTT broker
-      else:
-        with open("/home/pi/Serban_Apostol/temporary.log", "a") as tmp:
-          tmp.write(json.dumps(info)+"\n")
-      time.sleep(5) # Sleep for 5 seconds
-  except Exception as e:
-    print("Exception: " + str(e))
+  temp = str(check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True)) # Get CPU temperature from bash
+  info = {"CPU_temp":temp[7:11]}
+  print(info)
+  with open("/home/pi/Serban_Apostol/CPU_temp.log", "a") as log:
+    log.write(str(info)+"\n")
+  if is_connected:
+    ourClient.publish("training/device/Serban_Apostol", json.dumps(info)) # Publish time and CPU temperature to MQTT broker
+  else:
+    with open("/home/pi/Serban_Apostol/temporary.log", "a") as tmp:
+      tmp.write(json.dumps(info)+"\n")
+  time.sleep(10) # Sleep for 10 seconds
